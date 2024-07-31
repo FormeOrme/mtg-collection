e1ab57f26aa2325a0c282e96e2318a4d15b0694c
Federico Formenti <34321372+FormeOrme@users.noreply.github.com>
2024-07-31 10:47:26 +0200

    scryfall barebone update;

--- scryfall_barebone.js
+++ scryfall_barebone.js
@@ -2,27 +2,26 @@ const fs = require('fs');
 const path = require('path');
 const common = require(path.join(__dirname, 'common'));
 
-const bareboneFilename = "scryfall_barebone.json";
+const bareboneFilename = "scryfall_barebone";
 
 const base_art_url = "https://cards.scryfall.io/art_crop";
 
 function img(image_uris, card_faces) {
-    if (card_faces) {
-        image_uris = card_faces[0].image_uris
+    let uris = image_uris;
+    if (!uris && card_faces) {
+        uris = card_faces[0].image_uris;
     }
-    return image_uris?.art_crop.replace(base_art_url, "") || "none";
+    return uris?.art_crop.replace(base_art_url, "");
 }
 
 const barebone = common.defaultData
     .filter(card => Object.values(card.legalities).some(value => value == "legal")
         && !["token", "funny", "memorabilia", "promo"].includes(card.set_type))
     .map(({ name, set, collector_number, color_identity, image_uris, card_faces }) =>
-        ({ name, set, cn: collector_number, ci: color_identity.join(), img: img(image_uris, card_faces) }))
+        ({ name, set, cn: collector_number, ci: color_identity.join(""), img: img(image_uris, card_faces) }))
     .sort((c1, c2) => c1.name.localeCompare(c2.name))
-const bareboneContent = JSON.stringify(barebone);
-fs.writeFile(bareboneFilename, bareboneContent, function (err) {
+
+fs.writeFile(bareboneFilename + ".json", JSON.stringify(barebone), function (err) {
     if (err) return console.log(err);
     // console.log(shrunkData);
 });
-
- 
