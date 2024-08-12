a45bc9d7aa67f77cc0d1931cc26a97c0816e187b
Federico Formenti <34321372+FormeOrme@users.noreply.github.com>
2024-08-12 10:14:19 +0200

    added compressed file;

--- shrink_default.js
+++ shrink_default.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const common = require(path.join(__dirname, 'common'));
+const zlib = require('zlib');
 
 const strip = (s) => s.split("/")[0]?.trim().replace(/\W+/g, "_").toLowerCase();
 
@@ -13,14 +14,25 @@ const shrunkData = common.oracleData.filter(common.legalCards).map(c => ({
     //set: c.set,
     // slashes: c.name.includes("/") ? SLASHES[c.set] : undefined,
     r: c.rarity[0],
-    a: c.games.includes("arena")?1:undefined
+    a: c.games.includes("arena") ? 1 : undefined
 })).sort((c1, c2) => c1.n.localeCompare(c2.n));
 console.timeEnd("shrunkData");
 
 const shrunkFileName = "scryfall_arena_data.json";
 console.log(`[${shrunkFileName}] contains [${shrunkData.length}] cards`);
 const shrunkString = JSON.stringify(shrunkData);
+
 fs.writeFile(shrunkFileName, shrunkString, function (err) {
-    if (err) return console.log(err);
-    // console.log(shrunkData);
+    if (err) console.log(err);
+});
+
+zlib.gzip(shrunkString, (err, buffer) => {
+    if (err) {
+        console.log('Error compressing the data:', err);
+        return;
+    }
+    fs.writeFile(shrunkFileName + '.gz', buffer, (err) => {
+        if (err) console.log('Error writing the file:', err)
+        else console.log('File successfully written and compressed.');
+    });
 });
