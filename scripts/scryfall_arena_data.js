f7e0f32141c26637f61a6b1e661e4eee816d3445
Federico Formenti <federico.formenti.0@gmail.com>
2025-07-20 17:09:19 +0200

    Data update

--- scripts/scryfall_arena_data.js
+++ scripts/scryfall_arena_data.js
@@ -11,42 +11,51 @@ import {
 
 const rarities = { c: 0, u: 1, r: 2, m: 3 };
 
+console.time("Loading oracle cards");
 const oracleCards = oracleData();
 console.log(`Found ${oracleCards.length} oracle cards.`);
 
 const filtered = oracleCards.filter(legalCards);
 console.log(`Filtered to ${filtered.length} legal cards.`);
+console.timeEnd("Loading oracle cards");
+
+function mapCardData(card, additionalData = false) {
+    const baseData = {
+        n: strip(card.name),
+        r: rarities[card.rarity[0]],
+        ...(onArena(card.legalities) && { a: 1 }),
+        ...(modernLegal(card.legalities) && { m: 1 }),
+        ci: colorIdentity(card.color_identity),
+        ...(sets.straightToModern.includes(card.set) && { stm: true }),
+    };
+
+    if (additionalData) {
+        return {
+            ...baseData,
+            id: card.id
+        };
+    }
+
+    return baseData;
+}
 
 console.time("shrunkData");
-const shrunkData = filtered.map(({ name, set, rarity, color_identity, legalities }) => ({
-    n: strip(name),
-    r: rarities[rarity[0]],
-    ...(onArena(legalities) && { a: 1 }),
-    ...(modernLegal(legalities) && { m: 1 }),
-    ci: colorIdentity(color_identity),
-    ...(sets.straightToModern.includes(set) && { stm: true }),
-}));
+const shrunkData = filtered.map((card) => mapCardData(card));
 console.timeEnd("shrunkData");
 
 const shrunkString = JSON.stringify(shrunkData);
 
-writeToData(shrunkString, "scryfall_arena_data.json");
+const OUTPUT_FILES = {
+    SHRUNK: "scryfall_arena_data.json",
+    BAREBONE: "scryfall_barebone.json",
+};
+
+writeToData(shrunkString, OUTPUT_FILES.SHRUNK);
 
 console.time("bareboneData");
-const bareboneData = filtered.map(
-    ({ name, set, rarity, color_identity, legalities, image_uris, card_faces }) => ({
-        n: strip(name),
-        r: rarities[rarity[0]],
-        ...(onArena(legalities) && { a: 1 }),
-        ci: colorIdentity(color_identity),
-        ...(sets.straightToModern.includes(set) && { stm: true }),
-        ...(image_uris?.border_crop || card_faces?.[0]?.image_uris?.border_crop
-            ? { img: image_uris?.border_crop || card_faces?.[0]?.image_uris?.border_crop }
-            : {}),
-    }),
-);
+const bareboneData = filtered.map((card) => mapCardData(card, true));
 console.timeEnd("bareboneData");
 
 const bareboneString = JSON.stringify(bareboneData);
 
-writeToData(bareboneString, "scryfall_barebone.json");
+writeToData(bareboneString, OUTPUT_FILES.BAREBONE);
